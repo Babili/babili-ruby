f6ddcbbd4e96ea8d059dd702acf883a56b2dbe02
Marc Lainez <marc.lainez@gmail.com>
2016-04-08 13:31:13 +0200

    Add missing user actions

--- lib/babili/platform/user.rb
+++ lib/babili/platform/user.rb
@@ -1,10 +1,10 @@
 module Babili
   module Platform
     class User < OpenStruct
-      @path = "platform/users"
+      @@path = "platform/users"
 
       def self.all
-        raw_users = Babili::Client.get(@path)
+        raw_users = Babili::Client.get(path)
         raw_users["data"].map do |raw_user|
           user    = self.new(raw_user["attributes"])
           user.id = raw_user["id"]
@@ -13,10 +13,24 @@ module Babili
       end
 
       def self.create(params = {})
-        raw_user = Babili::Client.post(@path, params)["data"]
+        raw_user = Babili::Client.post(path, params)["data"]
         user     = self.new(raw_user["attributes"])
         user.id  = raw_user["id"]
         user
+      end
+
+      def delete
+        path     = self.class.path + "/#{id}"
+        raw_user = Babili::Client.delete(path)["data"]
+        user     = self.class.new(raw_user["attributes"])
+        user.id  = raw_user["id"]
+        user
+      end
+
+      private
+
+      def self.path
+        @@path
       end
     end
   end
--- spec/platform/user_spec.rb
+++ spec/platform/user_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Babili::Platform::User, ".all" do
       expect(Babili::Platform::User.all.size).to eq(0)
     end
   end
+
+  context "when is a user" do
+    before(:each) do
+      stub_request(:get, "https://babili-api.spin42.me/platform/users")
+        .to_return(body: '{"data": [{"type": "user", "data": {"id": 1234}}], "meta": { "count": 1 } }')
+    end
+
+    it "should not return an empty array" do
+      expect(Babili::Platform::User.all.size).to eq(1)
+    end
+  end
 end
